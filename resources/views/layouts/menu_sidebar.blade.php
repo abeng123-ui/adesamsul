b601809ac1b61281688d12bfe1cd1d6fd22b5297
akbarsyidiqi <akbarsyidiq@jojonomic.com>
2020-11-30 20:37:55 +0700

    first commit

---
+++ resources/views/layouts/menu_sidebar.blade.php
@@ -0,0 +1,49 @@
+<!--
+  Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+  Tip 2: you can also add an image using data-image tag
+-->
+<div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
+    Kependudukan
+  </a></div>
+<div class="sidebar-wrapper">
+  <ul class="nav">
+    <li class="nav-item  ">
+      <a class="nav-link" href="{{ url('/') }}">
+        <i class="material-icons">dashboard</i>
+        <p>Beranda</p>
+      </a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link" href="./user.html">
+        <i class="material-icons">person</i>
+        <p>Master Data</p>
+      </a>
+    </li>
+    <li class="nav-item active ">
+      <a class="nav-link" href="./tables.html">
+        <i class="material-icons">content_paste</i>
+        <p>Penduduk</p>
+      </a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link" href="./typography.html">
+        <i class="material-icons">library_books</i>
+        <p>Manajemen Akses</p>
+      </a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link" href="./icons.html">
+        <i class="material-icons">bubble_chart</i>
+        <p>Manajemen Desa</p>
+      </a>
+    </li>
+    <!-- <li class="nav-item ">
+      <a class="nav-link" href="./map.html">
+        <i class="material-icons">location_ons</i>
+        <p>Surat Keterangan</p>
+      </a>
+    </li> -->
+
+  </ul>
+</div>
